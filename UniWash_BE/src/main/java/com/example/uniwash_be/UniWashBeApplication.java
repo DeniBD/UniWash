feb021dda9a6a502fabe040550d2bd38ac37bb3d
DeniBD <denisa.buftea02@e-uvt.ro>
2023-11-01 20:28:14 +0200

    Project Setup

---
+++ UniWash_BE/src/main/java/com/example/uniwash_be/UniWashBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.uniwash_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniWashBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniWashBeApplication.class, args);
+    }
+
+}
