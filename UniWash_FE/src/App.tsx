5184caac0f82a86d845cdc7ce37e39440f51612a
DeniBD <denisa.buftea02@e-uvt.ro>
2023-12-06 02:45:47 +0200

    Created LoginPage

--- UniWash_FE/src/App.tsx
+++ UniWash_FE/src/App.tsx
@@ -4,12 +4,14 @@ import UserDashboard from "./App/Screens/UserDashboard/UserDashboard";
 import { Route, Routes } from "react-router";
 import Reservations from "./App/Screens/Reservations/Reservations";
 import Messages from "./App/Screens/Messages/Messages";
+import Login from './App/Screens/Login/Login';
 
 function App() {
   return (
     <div>
       <Routes>
-          <Route path = "/" element = {<UserDashboard/>}/>
+          <Route path = "/" element = {<Login/>}/>
+          <Route path = "/dashboard" element = {<UserDashboard/>}/>
           <Route path = "/reservations" element={<Reservations/>}/>
           <Route path = "/messages" element={<Messages/>}/>
       </Routes>
